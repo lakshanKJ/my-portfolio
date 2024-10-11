b127b0ac08915585635de240edbf100ef91c16dd
LakshanKJ <lakshan,kj001@gmail.com>
2024-10-12 05:08:45 +0530

    test2

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -2,7 +2,7 @@ import styles from './Contact.module.css';
 
 function Contact() {
     return(
-        <div className={styles.main}><p>Email: <link>lakshan.kj001@gmail.com</link></p></div>
+        <div className={styles.main}><p>Email: <link>lakshan.kj001@gmail.com</link> changed content to test ci cd</p></div>
     );
 }
 
