16b0176882c212e055120f05ecc8958c1b05300b
LakshanKJ <lakshan,kj001@gmail.com>
2024-10-12 06:37:37 +0530

    6

--- src/Projects/Streaming platform/Streaming.js
+++ src/Projects/Streaming platform/Streaming.js
@@ -2,7 +2,7 @@ import stream1 from "../../assets/images/stream1.png";
 import stream2 from "../../assets/images/stream2.png";
 import stream3 from "../../assets/images/stream3.png";
 import styles from './Streaming.module.css';
-import React from "react";
+
 
 function StreamPlatform() {
     return (
