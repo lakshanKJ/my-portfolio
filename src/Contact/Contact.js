81d944f2d1bcfc32d8a346caf42fc34d3101e3a4
LakshanKJ <lakshan,kj001@gmail.com>
2024-10-12 07:01:25 +0530

    8

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -2,7 +2,7 @@ import styles from './Contact.module.css';
 
 function Contact() {
     return(
-        <div className={styles.main}><p>Email: lakshan.kj001@gmail.com</p></div>
+        <div className={styles.main}><p>Email: lakshan.kj001@gmail.com changed to test</p></div>
     );
 }
 
